65ade33ee843e2f29910a4c037596a0aa98e684c
Justus Perlwitz <mail@justusperlwitz.com>
2015-08-28 23:23:24 +0200

    ADD: lay fundament

--- gon.py
+++ gon.py
@@ -1,7 +1,41 @@
-from flask import Flask
+from os import environ
+from time import time
+from random import randint
+
+from flask import (
+    Flask,
+    flash,
+    render_template,
+    redirect,
+)
+
+from images import get_images
 
 app = Flask(__name__)
+app.secret_key = environ['SECRET_KEY']
+
 
 @app.route("/")
 def index():
-    return "Hello World"
+    """
+    Index.
+
+    Retriggers cache for image retrieval once every 60 hours
+    """
+    cache_var = lambda: int(time() / 3600)
+    images = list(get_images(cache_var()))
+    return render_template(
+        'index.html',
+        image_a=images.pop(randint(0, len(images))),
+        image_b=images.pop(randint(0, len(images))),
+    )
+
+
+@app.route("/vote")
+def vote():
+    flash('Thank you for voting!')
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ images.py
@@ -0,0 +1,30 @@
+from functools import lru_cache
+from os import getenv
+from imgurpython import ImgurClient
+from imgurpython.imgur.models.gallery_image import GalleryImage
+
+
+client_id = getenv('IMGUR_CLIENT_ID')
+client_secret = getenv('IMGUR_CLIENT_SECRET')
+
+client = ImgurClient(client_id, client_secret)
+
+
+# Example request
+@lru_cache(maxsize=60)
+def get_images(hour):
+    """
+    Get animated images from imgur.
+
+    Caches 60 different versions.
+    """
+    return list(filter(
+        lambda item: item.animated, filter(
+            lambda item: isinstance(item, GalleryImage),
+            client.gallery())))
+
+
+if __name__ == "__main__":
+    for i in get_images():
+        print(i.__dict__)
+        print(i.link)
