53e0fe721975fe3674125b6abf5dfe31998adf4b
Justus Perlwitz <mail@justusperlwitz.com>
2015-08-31 16:47:18 +0200

    ADD: Less noisy logging for rq worker

--- run-worker.py
+++ run-worker.py
@@ -1,6 +1,8 @@
 from os import getenv
+
 from redis import Redis
 from rq import Worker, Queue, Connection
+from rq.logutils import setup_loghandlers
 
 listen = ['default']
 
@@ -8,4 +10,5 @@ conn = Redis.from_url(getenv('REDIS_URL', 'redis://localhost:6379/rq'))
 
 with Connection(conn):
     worker = Worker(map(Queue, listen))
+    setup_loghandlers(level='WARNING')
     worker.work()
