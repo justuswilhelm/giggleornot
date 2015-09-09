0de2223e6474cb3b463254c69cb45b1770e758a1
Justus Perlwitz <mail@justusperlwitz.com>
2015-09-09 21:16:07 +0200

    ADD: Improve new user  creation tracking

--- tracking.py
+++ tracking.py
@@ -2,7 +2,10 @@
 from os import getenv
 from multiprocessing import Pool
 
-from flask import session
+from flask import (
+    request,
+    session,
+)
 from mixpanel import Mixpanel
 
 mp = Mixpanel(getenv('MIXPANEL_TOKEN'))
@@ -13,8 +16,24 @@ user_id = lambda: session.get('uid', 'anon')
 
 
 def track_vote(image_id, is_up):
-    pool.apply_async(mp.track, [user_id(), 'vote', {'image_id': image_id}])
+    pool.apply_async(
+        mp.track,
+        [user_id(), 'vote', {'image_id': image_id}]
+    )
 
 
 def track_new_user():
-    pool.apply_async(mp.track, [user_id(), 'new_user'])
+    pool.apply_async(
+        mp.track, [
+            user_id(),
+            'new_user',
+            {'$browser': request.user_agent.browser,
+             '$browser_version': request.user_agent.version,
+             '$initial_referrer': request.args.get('ref') or request.referrer,
+             },
+        ])
+    pool.apply_async(
+        mp.people_set, [
+            user_id(),
+            {},
+        ])
