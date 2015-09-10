b027746ac59c49e7823e39a659a322728964d5ca
Justus Perlwitz <mail@justusperlwitz.com>
2015-09-10 11:48:00 +0200

    ADD: Track browser in vote event

--- twogifs/tracking.py
+++ twogifs/tracking.py
@@ -18,7 +18,13 @@ user_id = lambda: session.get('uid', 'anon')
 def track_vote(up, down):
     pool.apply_async(
         mp.track,
-        [user_id(), 'vote', {'up': up, 'down': down}]
+        [user_id(), 'vote', {
+            'up': up,
+            'down': down,
+            '$browser': request.user_agent.browser,
+            '$browser_version': request.user_agent.version,
+            '$initial_referrer': request.args.get('ref') or request.referrer,
+        }]
     )
     pool.apply_async(
         mp.people_set,
@@ -34,9 +40,9 @@ def track_new_user():
     pool.apply_async(
         mp.track, [
             user_id(),
-            'new_user',
-            {'$browser': request.user_agent.browser,
-             '$browser_version': request.user_agent.version,
-             '$initial_referrer': request.args.get('ref') or request.referrer,
-             },
-        ])
+            'new_user', {
+                '$browser': request.user_agent.browser,
+                '$browser_version': request.user_agent.version,
+                '$initial_referrer': request.args.get('ref')
+                or request.referrer,
+            }])
