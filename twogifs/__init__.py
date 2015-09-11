d0e50017cef5713479e3b6c5ac215627e5287e0e
Justus Perlwitz <mail@justusperlwitz.com>
2015-09-11 15:15:54 +0200

    REM: Cache

--- twogifs/__init__.py
+++ twogifs/__init__.py
@@ -5,7 +5,6 @@ from os import (
 )
 
 from flask import Flask
-from flask.ext.cache import Cache
 from redis import Redis
 
 app = Flask(__name__)
@@ -18,11 +17,5 @@ app.config.from_object(__name__)
 app.db = Redis.from_url(
     getenv('REDIS_URL', 'redis://localhost:6379/'))
 app.secret_key = environ['SECRET_KEY']
-
-
-cache = Cache(app, config={
-    'CACHE_TYPE': 'redis',
-    'CACHE_REDIS_URL': getenv('REDIS_URL', 'redis://localhost:6379/'),
-})
 
 from . import views  # noqa
